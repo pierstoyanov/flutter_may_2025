47a86617103f42e7fb7a6e6252403341494a2ac8
Pier Stoyanov <pier.stoyanov@gmail.com>
2025-06-08 01:09:14 +0300

    add provider

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_application_1/providers/calendar_provider.dart';
 import 'package:flutter_application_1/screens/landing/landing_page.dart';
+import 'package:provider/provider.dart';
 
 void main() {
   runApp(const MyApp());
@@ -15,7 +17,10 @@ class MyApp extends StatelessWidget {
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
       ),
-      home: const LandingPage(title: 'Calendar'),
+      home: ChangeNotifierProvider(
+          create: (context) => CalendarProvider(DateTime.now()),
+          child: const LandingPage(title: 'Calendar'),
+        ), 
     );
   }
 }
