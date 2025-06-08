68f2e3e52b9d6cb4b1baf501c28fb65b2980981e
Pier Stoyanov <pier.stoyanov@gmail.com>
2025-06-08 23:10:24 +0300

    add week view screen

---
+++ lib/screens/week_view_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/widgets/my_app_bar.dart';
+import 'package:flutter_application_1/widgets/my_bottom_navigation_bar.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/providers/bottom_navigation_bar_provider.dart';
+
+class WeekViewScreen extends StatelessWidget {
+  final String title;
+  const WeekViewScreen({super.key, required this.title});
+
+  @override
+  Widget build(BuildContext context) {
+    final navbarProvider = context.watch<BottomNavigationBarProvider>();
+    return Scaffold(
+      appBar: MyAppBar(title: title),
+      body: const Center(
+        child: Text('Week View - Coming Soon!'),
+      ),
+      bottomNavigationBar: MyBottomNavigationBar(
+        currentIndex: navbarProvider.currentIndex,
+        onItemSelected: (index) => navbarProvider.navigateTo(index, context),
+      ),
+    );
+  }
+}
