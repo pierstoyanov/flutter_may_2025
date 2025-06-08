510430ca6888ef56a41272c1c939bc6473e6e4f0
Pier Stoyanov <pier.stoyanov@gmail.com>
2025-06-08 15:33:18 +0300

    update & move landing page

---
+++ lib/screens/landing_page.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_application_1/providers/bottom_navigation_bar_provider.dart';
+import 'package:flutter_application_1/widgets/app_bar.dart';
+import 'package:flutter_application_1/widgets/navbar.dart';
+import 'package:provider/provider.dart';
+import 'package:flutter_application_1/widgets/month_callendar/month_calendar.dart';
+
+class LandingPage extends StatefulWidget {
+  final String title;
+  const LandingPage({super.key, required this.title});
+
+  @override
+  State<LandingPage> createState() => _LandingPageState();
+}
+
+class _LandingPageState extends State<LandingPage> {
+  @override
+  Widget build(BuildContext context) {
+    // Use 'watch' to rebuild if currentIndex changes.
+    final navbarProvider = context.watch<BottomNavigationBarProvider>();
+    return Scaffold(
+      appBar: MyAppBar(title: widget.title),
+      body: ListView(
+        children: [
+          MonthCalendar(),
+          Padding(
+            padding: const EdgeInsets.all(20.0),
+            child: Container(
+              color: Colors.amber,
+              height: 250,
+            )
+          ),
+      ]),
+      bottomNavigationBar: MyBottomNavigationBar(
+        currentIndex: navbarProvider.currentIndex,
+        onItemSelected: (index) => navbarProvider.navigateTo(index, context),
+      ),
+    );
+  }
+}
