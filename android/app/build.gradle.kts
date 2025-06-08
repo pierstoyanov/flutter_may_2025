dc09a0a70bf02b985a5401c5343df95d572ee243
Pier Stoyanov <pier.stoyanov@gmail.com>
2025-06-08 18:19:42 +0300

    update build.gradle

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -27,11 +27,12 @@ android {
         applicationId = "com.example.flutter_application_1"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdkVersion(23)
+        ndkVersion = "27.0.12077973"
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
-        ndkVersion = "27.0.12077973"
+
     }
 
     buildTypes {
